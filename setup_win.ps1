128c29e6b96f7e9394d903bfc76925a9edf5fb97
dennisfsilva <dev.dennissilva@gmail.com>
2023-06-01 19:38:20 +0100

    enhanced setup wizard

--- setup_win.ps1
+++ setup_win.ps1
@@ -1,128 +1,158 @@
-# Function to display a loading animation with EVACLUE branding
-function Show-LoadingAnimation {
-    $spinner = @("/","-","\","|")
-    $i = 0
+# Prompt for root DB user and password
+$rootUser = Read-Host -Prompt "Enter the root DB user"
+$rootPassword = Read-Host -Prompt "Enter the root DB password" -AsSecureString
+$rootPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($rootPassword))
 
-    while ($true) {
-        $loadingText = "Processing... $($spinner[$i]) [EVACLUE]"
-        Write-Host -ForegroundColor Green "`r$loadingText"
-        $i = ($i + 1) % $spinner.Length
-        Start-Sleep -Milliseconds 200
+# Function to create a directory if it doesn't exist
+function CreateDirectoryIfNotExists($path) {
+    if (-not (Test-Path $path -PathType Container)) {
+        New-Item -Path $path -ItemType Directory -Force | Out-Null
     }
 }
 
-# Function to clear the loading animation
-function Clear-LoadingAnimation {
-    Write-Host "`r" -NoNewline
-    Clear-Host
+# Clear the console
+Clear-Host
+
+# Display Evaclue Setup Configuration
+Write-Host "╔══════════════════════════════════════╗" -ForegroundColor red
+Write-Host "║     EVACLUE - Setup Wizard v0.1      ║" -ForegroundColor red
+Write-Host "╚══════════════════════════════════════╝" -ForegroundColor red
+Write-Host
+
+# Prompt for MySQL database setup
+Write-Host "MySQL Database Setup" -ForegroundColor Yellow
+Write-Host "-------------------" -ForegroundColor Yellow
+Write-Host
+$mysqlSetup = @{
+    "DB_HOST"     = "localhost"
+    "DB_USER"     = $rootUser
+    "DB_PASSWORD" = $rootPassword
+    "DB_NAME"     = "evaclue_db"
 }
+$mysqlSetup.GetEnumerator() | ForEach-Object {
+    $key = $_.Key
+    $value = $_.Value
+    Write-Host "$key = $value"
+}
+Write-Host
 
-# Prompt for root DB user and password
-Write-Host -ForegroundColor Cyan "EVACLUE Project Deployment"
-Write-Host "Please enter the root DB user and password:"
-$rootUser = Read-Host -Prompt "User"
-$rootPassword = Read-Host -Prompt "Password" -AsSecureString
-$rootPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($rootPassword))
+# Go to the Database folder
+Set-Location -Path ".\Database"
 
-# Show loading animation while making connection to the MySQL database
-Write-Host "Connecting to the MySQL database..."
-Show-LoadingAnimation
+# Make connection to the MySQL database
 mysql -u $rootUser -p"$rootPassword" -e "source evaclue_db.sql"
-Clear-LoadingAnimation
 
-# Create logs folder in different directories
-Write-Host "Creating logs folders..."
+# Go back to the Root directory
+Set-Location -Path ".."
+
+# Prompt for logs folders
+Write-Host
+Write-Host "Logs Folders Setup" -ForegroundColor Yellow
+Write-Host "------------------" -ForegroundColor Yellow
+Write-Host
 $logsPath = @(
     ".\Services\NotificationService\logs",
     ".\Services\ReviewsService\logs",
     ".\Services\UsersService\logs"
 )
-
 foreach ($path in $logsPath) {
-    Show-LoadingAnimation
-    New-Item -Path $path -ItemType Directory -Force | Out-Null
-    Clear-LoadingAnimation
+    CreateDirectoryIfNotExists $path
+    Write-Host "Created logs folder: $path"
 }
+Write-Host
 
-# Prompt for dirnames
+# Prompt for Reviews Service setup
+Write-Host "Reviews Service Setup" -ForegroundColor Yellow
+Write-Host "---------------------" -ForegroundColor Yellow
+Write-Host
 $reviewsServiceDirname = Read-Host -Prompt "Enter Reviews Service resProofFiles DIRNAME"
+$reviewsServiceSetup = @{
+    "PORT"    = 8000
+    "DB_HOST" = "localhost"
+    "DB_USER" = $rootUser
+    "DB_PASSWORD" = $rootPassword
+    "DB_NAME" = "evaclue_db"
+    "APIKEY" = "AIzaSyBq2YyQh70n_M6glKgr3U4a9vCmY5LU0xQ"
+    "SECRET" = "greedisgood"
+    "DIRNAME" = $reviewsServiceDirname
+}
+$reviewsServiceSetup.GetEnumerator() | ForEach-Object {
+    $key = $_.Key
+    $value = $_.Value
+    Write-Host "$key = $value"
+}
+Write-Host
+
+# Prompt for Users Service setup
+Write-Host "Users Service Setup" -ForegroundColor Yellow
+Write-Host "--------------------" -ForegroundColor Yellow
+Write-Host
 $usersServiceDirname = Read-Host -Prompt "Enter Users Service userImages DIRNAME"
+$usersServiceSetup = @{
+    "SERVER_PORT" = 8001
+    "DB_HOST"     = "localhost"
+    "DB_USER"     = $rootUser
+    "DB_PASSWORD" = $rootPassword
+    "DB_NAME"     = "evaclue_db"
+    "JWT_SECRET"  = "greedisgood"
+    "DIRNAME"     = $usersServiceDirname
+}
+$usersServiceSetup.GetEnumerator() | ForEach-Object {
+    $key = $_.Key
+    $value = $_.Value
+    Write-Host "$key = $value"
+}
+Write-Host
 
-# Create environment files for ReviewsService
-Write-Host "Creating environment file for ReviewsService..."
-@"
-PORT = 8000
-DB_HOST = "localhost"
-DB_USER = "$rootUser"
-DB_PASSWORD = "$rootPassword"
-DB_NAME = "evaclue_db"
-APIKEY = "AIzaSyBq2YyQh70n_M6glKgr3U4a9vCmY5LU0xQ"
-SECRET = "greedisgood"
-DIRNAME = "$reviewsServiceDirname"
-"@ | Set-Content -Path ".\Services\ReviewsService\.env" -Encoding utf8
+# Prompt for Notification Service setup
+Write-Host "Notification Service Setup" -ForegroundColor Yellow
+Write-Host "--------------------------" -ForegroundColor Yellow
+Write-Host
+$notificationServiceSetup = @{
+    "PORT"         = 8002
+    "DB_HOST"      = "localhost"
+    "DB_USER"      = $rootUser
+    "DB_PASSWORD"  = $rootPassword
+    "DB_NAME"      = "evaclue_db"
+    "SECRET"       = "greedisgood"
+    "SMTP_EMAIL"   = "rentifyWD@gmail.com"
+    "SMTP_HOST"    = "smtp.gmail.com"
+    "SMTP_PORT"    = 587
+    "SMTP_USER"    = "rentifywd@gmail.com"
+    "SMTP_PASS"    = "kxjkqatwgtyefpzy"
+}
+$notificationServiceSetup.GetEnumerator() | ForEach-Object {
+    $key = $_.Key
+    $value = $_.Value
+    Write-Host "$key = $value"
+}
+Write-Host
 
-# Create environment files for UsersService
-Write-Host "Creating environment file for UsersService..."
-@"
-SERVER_PORT = 8001
-DB_HOST = "localhost"
-DB_USER = "$rootUser"
-DB_PASSWORD = "$rootPassword"
-DB_NAME = "evaclue_db"
-JWT_SECRET = "greedisgood"
-DIRNAME = "$usersServiceDirname"
-"@ | Set-Content -Path ".\Services\UsersService\.env" -Encoding utf8
-
-# Create environment files for UsersService/database
-Write-Host "Creating environment file for UsersService/database..."
-@"
-SERVER_PORT = 8001
-DB_HOST = "localhost"
-DB_USER = "$rootUser"
-DB_PASSWORD = "$rootPassword"
-DB_NAME = "evaclue_db"
-JWT_SECRET = "greedisgood"
-"@ | Set-Content -Path ".\Services\UsersService\database\.env" -Encoding utf8
-
-# Create environment files for NotificationService
-Write-Host "Creating environment file for NotificationService..."
-@"
-PORT = 8002
-DB_HOST = "localhost"
-DB_USER = "$rootUser"
-DB_PASSWORD = "$rootPassword"
-DB_NAME = "evaclue_db"
-SECRET = "greedisgood"
-SMTP_EMAIL = "rentifyWD@gmail.com"
-SMTP_HOST = "smtp.gmail.com"
-SMTP_PORT = 587
-SMTP_USER = "rentifywd@gmail.com"
-SMTP_PASS = "kxjkqatwgtyefpzy"
-"@ | Set-Content -Path ".\Services\NotificationService\.env" -Encoding utf8
-
-# Create environment files for userclient
-Write-Host "Creating environment file for userclient..."
-@"
-VUE_APP_SERVER_PORT=8001
-"@ | Set-Content -Path ".\Views\userclient\.env" -Encoding utf8
+# Prompt for User Client setup
+Write-Host "User Client Setup" -ForegroundColor Yellow
+Write-Host "-----------------" -ForegroundColor Yellow
+Write-Host
+$userClientSetup = @{
+    "VUE_APP_SERVER_PORT" = 8001
+}
+$userClientSetup.GetEnumerator() | ForEach-Object {
+    $key = $_.Key
+    $value = $_.Value
+    Write-Host "$key = $value"
+}
+Write-Host
 
 # Install gulp globally
-Write-Host "Installing gulp globally..."
-Show-LoadingAnimation
+Write-Host "Installing gulp globally..." -ForegroundColor Cyan
 npm install --global gulp-cli
-Clear-LoadingAnimation
 
 # Install project dependencies
-Write-Host "Installing project dependencies..."
-Show-LoadingAnimation
+Write-Host "Installing project dependencies..." -ForegroundColor Cyan
 npm install
-Clear-LoadingAnimation
 
 # Build the application
-Write-Host "Building the application..."
-Show-LoadingAnimation
+Write-Host "Building the application..." -ForegroundColor Cyan
 gulp build
-Clear-LoadingAnimation
 
-# Finished
-Write-Host -ForegroundColor Green "Deployment complete! [EVACLUE]"
+Write-Host
+Write-Host "Setup configuration completed successfully!" -ForegroundColor Green

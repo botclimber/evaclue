f2ef900891dc4ffff6557635e7f13139ce371f8e
dennisfsilva <dev.dennissilva@gmail.com>
2023-06-22 15:35:14 +0000

    changing not request url

--- Views/evaclue-landingPage/static/js/subs.js
+++ Views/evaclue-landingPage/static/js/subs.js
@@ -1,7 +1,6 @@
 "use strict"
 
-//const SubApi = "http://evaclue.com:8002"
-const SubApi = "http://localhost:8002"
+const SubApi = "http://evaclue.com:8002"
 
 const subEmail = document.getElementById("subEmail")
 const subStatus = document.getElementById("subStatus")

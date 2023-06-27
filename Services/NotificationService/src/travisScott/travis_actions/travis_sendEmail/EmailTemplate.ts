c1150a371a0c8163e9729fd80d9a05763e6ac427
dennisfsilva <dev.dennissilva@gmail.com>
2023-06-27 16:54:08 +0000

    fix of url in the subscription email

--- Services/NotificationService/src/travisScott/travis_actions/travis_sendEmail/EmailTemplate.ts
+++ Services/NotificationService/src/travisScott/travis_actions/travis_sendEmail/EmailTemplate.ts
@@ -55,20 +55,8 @@ export class EmailTemplate {
                 <tr>
                   <td style="padding:0;width:50%;" align="left">
                     <p style="margin:0;font-size:14px;line-height:16px;font-family:Arial,sans-serif;color:#ffffff;">
-                      &reg; Evaclue, 2023<br/><a href="http://www.example.com" style="color:#ffffff;text-decoration:underline;">https://www.evaclue.com</a>
+                      &reg; Evaclue, 2023<br/><a href="https://www.evaclue.com" style="color:#ffffff;text-decoration:underline;">https://www.evaclue.com</a>
                     </p>
-                  </td>
-                  <td style="padding:0;width:50%;" align="right">
-                    <table role="presentation" style="border-collapse:collapse;border:0;border-spacing:0;">
-                      <tr>
-                        <td style="padding:0 0 0 10px;width:38px;">
-                          <a href="http://www.twitter.com/" style="color:#ffffff;"><img src="https://assets.codepen.io/210284/tw_1.png" alt="Twitter" width="38" style="height:auto;display:block;border:0;" /></a>
-                        </td>
-                        <td style="padding:0 0 0 10px;width:38px;">
-                          <a href="http://www.facebook.com/" style="color:#ffffff;"><img src="https://assets.codepen.io/210284/fb_1.png" alt="Facebook" width="38" style="height:auto;display:block;border:0;" /></a>
-                        </td>
-                      </tr>
-                    </table>
                   </td>
                 </tr>
               </table>
@@ -138,20 +126,8 @@ export class EmailTemplate {
                 <tr>
                   <td style="padding:0;width:50%;" align="left">
                     <p style="margin:0;font-size:14px;line-height:16px;font-family:Arial,sans-serif;color:#ffffff;">
-                      &reg; Evaclue, 2023<br/><a href="http://www.example.com" style="color:#ffffff;text-decoration:underline;">https://www.evaclue.com</a>
+                      &reg; Evaclue, 2023<br/><a href="https://www.evaclue.com" style="color:#ffffff;text-decoration:underline;">https://www.evaclue.com</a>
                     </p>
-                  </td>
-                  <td style="padding:0;width:50%;" align="right">
-                    <table role="presentation" style="border-collapse:collapse;border:0;border-spacing:0;">
-                      <tr>
-                        <td style="padding:0 0 0 10px;width:38px;">
-                          <a href="http://www.twitter.com/" style="color:#ffffff;"><img src="https://assets.codepen.io/210284/tw_1.png" alt="Twitter" width="38" style="height:auto;display:block;border:0;" /></a>
-                        </td>
-                        <td style="padding:0 0 0 10px;width:38px;">
-                          <a href="http://www.facebook.com/" style="color:#ffffff;"><img src="https://assets.codepen.io/210284/fb_1.png" alt="Facebook" width="38" style="height:auto;display:block;border:0;" /></a>
-                        </td>
-                      </tr>
-                    </table>
                   </td>
                 </tr>
               </table>

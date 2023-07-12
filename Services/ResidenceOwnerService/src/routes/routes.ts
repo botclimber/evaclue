b06ca02355b4deb690cc470f542dd408b8e115ab
Walter Palma <WalterPalma@criticalmanufacturing.com>
2023-07-12 15:31:53 +0100

    Revert "first draft"
    This reverts commit f3c54c84f54b3d5ec3638be9bcd90c7084c89842.

--- Services/ResidenceOwnerService/src/routes/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { ResidenceOwnerController } from "../controllers/ResindenceOwnerController";
-// import { authMiddleware } from "../middlewares/authMiddleware";
-
-const routes = Router();
-
-routes.get("/GetAllClaimedResidence", new ResidenceOwnerController().GetAllClaimedResidence);
-
-export default routes;

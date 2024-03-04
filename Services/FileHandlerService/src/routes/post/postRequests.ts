73f6ae323abb6f6adeefdd46c5be6331a8b55749
Daniel Silva <daniel.silva.prg@gmail.com>
2024-03-04 13:42:00 +0000

    added token validation to fileHandler service and fixed node version for all services to align compatiblity

--- Services/FileHandlerService/src/routes/post/postRequests.ts
+++ Services/FileHandlerService/src/routes/post/postRequests.ts
@@ -1,12 +1,11 @@
 import { Response, Router } from "express";
 import { FileHandlerController } from "../../controllers/FileHandlerController";
-import {authMiddleware} from "../../../../CommonUtils/src/middlewares/authMiddleware";
+import { authMiddleware } from "../../../../CommonUtils/src/middlewares/authMiddleware";
 
 const FH: FileHandlerController = new FileHandlerController();
 
 const postRoutes = Router();
 
-// TODO: add token verification and check if user who is trying to manage files is the owner
 postRoutes.post("/addReviewImgs", authMiddleware, FH.addReviewImgs)
 postRoutes.post("/addResImgs", authMiddleware, FH.addResImgs)
 postRoutes.post("/addResDoc", authMiddleware, FH.addResDoc)

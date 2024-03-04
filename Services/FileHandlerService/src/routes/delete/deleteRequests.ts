1f6bb37a705072a8a5d6eb6b76e0346fe23af04a
Daniel Silva <daniel.silva.prg@gmail.com>
2024-03-04 12:43:19 +0000

    token validation added to fileHandler service

--- Services/FileHandlerService/src/routes/delete/deleteRequests.ts
+++ Services/FileHandlerService/src/routes/delete/deleteRequests.ts
@@ -1,11 +1,12 @@
 import { Response, Router } from "express";
 import { FileHandlerController } from "../../controllers/FileHandlerController";
+import {authMiddleware} from "../../../../CommonUtils/src/middlewares/authMiddleware";
 
 const FH: FileHandlerController = new FileHandlerController();
 
 const deleteRoutes = Router();
 
 // TODO: token verification
-deleteRoutes.delete("/delResFile/:id/:imgNr", FH.deleteResFile)
+deleteRoutes.delete("/delResFile/:id/:imgNr", authMiddleware, FH.deleteResFile)
 
 export default deleteRoutes
--- Services/FileHandlerService/src/routes/post/postRequests.ts
+++ Services/FileHandlerService/src/routes/post/postRequests.ts
@@ -1,15 +1,16 @@
 import { Response, Router } from "express";
 import { FileHandlerController } from "../../controllers/FileHandlerController";
+import {authMiddleware} from "../../../../CommonUtils/src/middlewares/authMiddleware";
 
 const FH: FileHandlerController = new FileHandlerController();
 
 const postRoutes = Router();
 
 // TODO: add token verification and check if user who is trying to manage files is the owner
-postRoutes.post("/addReviewImgs", FH.addReviewImgs)
-postRoutes.post("/addResImgs", FH.addResImgs)
-postRoutes.post("/addResDoc", FH.addResDoc)
-postRoutes.post("/addTicketAttach", FH.addTicketAttachment)
-postRoutes.post("/changeUserProfileImg", FH.changeUserProfileImg)
+postRoutes.post("/addReviewImgs", authMiddleware, FH.addReviewImgs)
+postRoutes.post("/addResImgs", authMiddleware, FH.addResImgs)
+postRoutes.post("/addResDoc", authMiddleware, FH.addResDoc)
+postRoutes.post("/addTicketAttach", authMiddleware, FH.addTicketAttachment)
+postRoutes.post("/changeUserProfileImg", authMiddleware, FH.changeUserProfileImg)
 
 export default postRoutes
